1cd7c5304ef81733c2be5195d9680bf802ba9e68
Kristen V <kristenvelardi@gmail.com>
2023-09-23 08:50:31 -0400

    added basic code to start program

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("Let's get this party started!");
+function getUserInput(){
+    var inputValue = document.querySelector("input").value;
+    return inputValue;
+}
+    document.querySelector(submit).addEventListener("click", function (){
+        var inputValue= document.querySelector("input").value;
+        var userInput= getUserInput();
+
+
+})
