7dc5f0fa4dd28b4f036c2070b43fcbd45483464e
Kristen V <kristenvelardi@gmail.com>
2023-09-23 10:35:02 -0400

    created toDo list and started code

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-console.log("Let's get this party started!");
+// console.log("Let's get this party started!");
+
+//a function to gather the input data from the user
 function getUserInput(){
-    var inputValue = document.querySelector("input").value;
-    return inputValue;
-}
-    document.querySelector(submit).addEventListener("click", function (){
-        var inputValue= document.querySelector("input").value;
-        var userInput= getUserInput();
+    var inputValue = document.getElementById("search").value;  
+    console.log (inputValue).value;
+};
 
 
-})
+
+
+//
+//-tie "search-gif" to submit button
+//-make submit button clickable
+//-send inputValue data through API using a "get request"
+//-"post" returned values in the "result-container"
+//-make "clear" button remove contents from the page
