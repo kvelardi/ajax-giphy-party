6efa0d5497f2b3b328a2410d9b72a803bfaadf3a
Kristen V <124750746+kvelardi@users.noreply.github.com>
2023-10-09 19:23:50 -0400

    the functioning app

--- app.js
+++ app.js
@@ -1,24 +1,44 @@
-console.log("Let's get this party started!");
+// console.log("Let's get this party started!");
 
-//a function to gather the input data from the user
-function userInput (){
-    const submitBtn = document.getElementById("submit");
-    const search = document. getElementById("search");
+const search = document. getElementById("search");
+
+const submitBtn = document.getElementById("submitBtn");
+
+// axios
+async function getData(){
+
+    giphy =`https://api.giphy.com/v1/gifs/search?q=${search.value}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`
+
+const response = await axios.get(giphy);
+
+const data = response.data.data;
+    console.log (data);
+
+    let i = [];
+// create a loop to loop through the data array
+for (i = 0; i < data.length; i++){
     
-    submitBtn.addEventListener("click", function (e){
-        e.preventDefault();
-        console.log (search.value)  
-    });
+        // inside of the loop create an img tag
+        const results = document.getElementById ('results');
+
+        const img = document.createElement ('img');
+
+        // use what ever variable u create for the image with a .src = <address of the url so something like data[i].images.medium_image.url
+        img.src = data[i].images.original.url;
+
+        // // use inerHTML to push the img to the actual html page
+        results.appendChild(img);
+
+     
+    }
 };
 
-userInput();
+// console.log ('this line is after axios');
 
 
-//
-//-link "search" to submit button
-//-make submit button clickable
-//-send inputValue data through API using a "get request"
-//-"post" returned values in the "result-container"
-//-make "clear" button remove contents from the page
+submitBtn.addEventListener("click", function (evt){
+    evt.preventDefault();
+    getData();
+});
 
 
